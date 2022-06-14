408192e8e085badfd5eead3c49622c1ae3a2623e
smansoccer <smansoccer@gmail.com>
2022-06-13 22:31:34 -0500

    e

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
 document.getElementById("start").addEventListener("click", function(){
-    let location = window.location.href;
-    let localLocation = location.substring(location.indexOf("/", 7), location.length)
-    window.location.assign("https://api.digikey.com/v1/oauth2/authorize?response_type=code&client_id=" + document.getElementById("clientId").value + "&redirect_uri=" + encodeURIComponent("https://google.com"))
+    window.location.assign("https://api.digikey.com/v1/oauth2/authorize?response_type=code&client_id=" + document.getElementById("clientId").value + "&redirect_uri=" + encodeURIComponent("https://smansoccer.github.io/Digikey-API"))
 })
